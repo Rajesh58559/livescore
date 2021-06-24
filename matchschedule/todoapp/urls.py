940e0d3630d9e566e46764eba26bfe477b56ddb3
Rajesh58559 <80101357+Rajesh58559@users.noreply.github.com>
2021-06-24 00:57:03 -0400

    Update urls.py

--- matchschedule/todoapp/urls.py
+++ matchschedule/todoapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
-app_name='todoapp'
+
+app_name = 'todoapp'
 urlpatterns = [
-    path('',views.home,name="home"),
-    path('delete/<int:id>/',views.delete,name='delete'),
+    path('', views.home, name="home"),
+    path('delete/<int:id>/', views.delete, name='delete'),
+    path('update/<int:id>/', views.update, name='update'),
 ]
